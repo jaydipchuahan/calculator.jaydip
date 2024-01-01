2b8f8979283e5b80dc6f584158283815e3557a2f
henil27 <155354207+henil27@users.noreply.github.com>
2024-01-01 14:26:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-function pick(val){
-    document.getElementById("Display").value +=val;
-
+function btnClick(val){
+    document.getElementById("display").value +=val;
 }
 function solve(){
-    let x=document.getElementById("Display").value;
+    let x=document.getElementById("display").value;
     let y=eval(x);
-    document.getElementById("Display").value=y;
+    document.getElementById("display").value=y;
 }
-function clr(){
-    document.getElementById("Display").value="";
+function Clr(){
+    document.getElementById("display").value="";
 }
-function Min(){
-    document.getElementById("Display").value --;
-}
+function Dlt() {
+    let display = document.getElementById('display').value;
+    display = display.slice(0, -1);
+    document.getElementById('display').value = display;
+  }
